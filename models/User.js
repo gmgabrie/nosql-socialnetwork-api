b8f0caa1c609c1382729fe131ab5a2f90ffb593e
gmgabrie <ggabriel420@gmail.com>
2023-06-23 10:15:14 -0400

    finished User model

---
+++ models/User.js
@@ -0,0 +1,40 @@
+const { Schema, model } = require("mongoose");
+
+// Schema to create User model
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trimmed: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+\@.+\..+/],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+const User = model("User", UserSchema);
+
+module.exports = User;
